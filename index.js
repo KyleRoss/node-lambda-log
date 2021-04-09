3accf0ea735a7502216ac62e727b74976854dc1d
Kyle Ross <kylerross1324@gmail.com>
2021-04-09 16:08:44 -0400

    chore: update jsdocs in index

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-/**
- * @module lambda-log
- */
-
 const LambdaLog = require('./lib/LambdaLog');
 /**
- * Instance of the [LambdaLog](#lambdalog) class which is exported when calling `require('lambda-log')`.
- * For more advanced usage, you can create a new instance of the LambdaLog class via `new log.LambdaLog()`.
- * @type LambdaLog
- * @kind LambdaLog Instance
- * @alias module:lambda-log
- *
- * @example
- * const log = require('lambda-log');
- *
- * log.info('Hello world!');
+ * Instance of the LambdaLog class which is exported when calling `require('lambda-log')`. For more
+ * advanced usage, you can create a new instance of the LambdaLog class via `new log.LambdaLog()`.
+ * @type {LambdaLog}
  */
 const log = new LambdaLog();
 
