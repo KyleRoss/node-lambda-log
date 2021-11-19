d3408a583bf67b03533e792bf63d9ed54c177081
Kyle Ross <kylerross1324@gmail.com>
2021-11-19 10:26:18 -0500

    chore: add separate tsconfig for tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,20 +1,26 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  testRegex: '\\.test\\.ts$',
+  testRegex: '\\.spec\\.ts$',
   resolver: 'jest-ts-webcompat-resolver',
   collectCoverage: true,
   coverageReporters: [['json', { file: 'report.json' }], 'lcov', 'text', 'text-summary', 'html'],
   collectCoverageFrom: ['src/**/*.ts'],
   coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/', '<rootDir>/src/typings.ts'],
+  coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/', '<rootDir>/src/index.ts', '<rootDir>/src/typings.ts'],
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
   moduleDirectories: ['node_modules', 'src'],
-  watchPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/', '<rootDir>/site/']
+  watchPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/', '<rootDir>/site/'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.spec.json'
+    }
+  }
 };
 
 export default config;
